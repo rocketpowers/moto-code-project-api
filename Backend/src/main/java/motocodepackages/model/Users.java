9a4b6bb046d45043afef5b7577a2550a91e222f7
rocketpowers <rocketpowerswillian@gmail.com>
2023-03-05 21:37:08 -0300

    ,h2 and mysql teste and on work

--- Backend/src/main/java/motocodepackages/model/Users.java
+++ Backend/src/main/java/motocodepackages/model/Users.java
@@ -6,11 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data
-@Entity
-public class Users  {
 
-	
+@Entity
+public class Users {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +24,48 @@ public class Users  {
 
 	private String phone;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
+	
+	
+	
 }
